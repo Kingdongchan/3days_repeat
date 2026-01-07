0b0d165154190db05c8a26edb766c1547e2bdd80
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-07 22:50:20 +0900

    삭제 함수 - 인덱스로 삭제

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 #입력
-target_name = input()
-new_age = int(input())
+idx = int(input())
 
 #처리
 members = [
     {"name": "김동찬", "age": 27},
-    {"name": "공욱재", "age": 26}
+    {"name": "공욱재", "age": 26},
+    {"name": "정승훈", "age": 25}
 ]
 
-def update_by_name(members, target_name, new_age):
-    for member in members:
-        if member["name"] == target_name:
-            member["age"] = new_age
-
+def delete_by_index(members, idx):
+    members.pop(idx)
+    
 #출력
-update_by_name(members, target_name, new_age)
+delete_by_index(members, idx)
 print(members)
