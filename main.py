5fd63742c0e0f898751fad4ddabb5cea358bac84
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-07 23:20:20 +0900

    while과 함수 조합으로 반봅 메뉴

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 #입력
-menu = input()
-
+def show_menu():
+    print("1. 조회")
+    print("2. 종료")
+    
+def read_all(members):
+    for member in members:
+        print(member)
+        
 #처리
 members = [
     {"name": "김동찬", "age": 27}
 ]
 
-def read_all(members):
-    for member in members:
-        print(member)
+while True:
+    show_menu()
+    menu = input()
+    if menu =="1":
+        read_all(members)
+    elif menu == "2":
+        break
         
-def get_count(members):
-    return len(members)
-
-#출력
-if menu == "1":
-    read_all(members)
-elif menu == "2":
-    print(get_count(members))
