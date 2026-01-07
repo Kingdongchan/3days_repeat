63606be74e35a0c159a2fdfded4ffdb2c5d734c3
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-07 23:36:16 +0900

    CRUD 전체를 함수 기반으로 구성

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
 #입력
 def show_menu():
-    print("1. 조회")
-    print("2. 종료")
+    print("1. 추가")
+    print("2. 전체조회")
+    print("3. 수정")
+    print("4. 삭제")
+    print("5. 종료")
+    
+def add_member(members):
+    name = input()
+    age = int(input())
+    members.append({"name": name, "age": age})
     
 def read_all(members):
-    for member in members:
-        print(member)
+    for idx, member in enumerate(members):
+        print(idx, member["name"], member["age"])
         
+def update_member(members):
+    idx = int(input())
+    new_age = int(input())
+    members[idx]["age"] = new_age
+    
+def delete_member(members):
+    idx = int(input())
+    members.pop(idx)
+    
 #처리
 def main():
-    members = [
-        {"name": "김동찬", "age": 27}
-    ]
+    members = []
     while True:
         show_menu()
         menu = input()
         if menu == "1":
-            read_all(members)
+            add_member(members)
         elif menu == "2":
+            read_all(members)
+        elif menu == "3":
+            update_member(members)
+        elif menu == "4":
+            delete_member(members)
+        elif menu == "5":
             break
+        
 #출력
 main()
