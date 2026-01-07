78cbc66d2f5f198e77452ec215ff6ebca89ac313
1968-501 <kimdongchan@1968-501ui-MacBookAir.local>
2026-01-07 22:36:57 +0900

    전체 조회 함수 - 번호와 함께 출력

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 #처리
 members = [
     {"name": "김동찬", "age": 27},
-    {"name": "공우갲", "age": 26}
+    {"name": "공욱재", "age": 26}
 ]
 
-def read_all(members):
-    for member in members:
-        print(member)
-
+def read_all_with_index(index):
+    for idx, member in enumerate(members):
+        print(idx, member["name"], member["age"])
+        
 #출력
-read_all(members)
+read_all_with_index(members)
